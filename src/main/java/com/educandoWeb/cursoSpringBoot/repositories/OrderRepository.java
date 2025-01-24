ccad579ecf4c1061f0c1a232cbb41b62f9a8dd8b
Guilherme <guilherme.sousa.castro.2004@gmail.com>
2025-01-24 10:50:30 +0000

    Order, instant, iso 8601 pt2

---
+++ src/main/java/com/educandoWeb/cursoSpringBoot/repositories/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.educandoWeb.cursoSpringBoot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoWeb.cursoSpringBoot.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+	
+	
+	
+	
+
+}
