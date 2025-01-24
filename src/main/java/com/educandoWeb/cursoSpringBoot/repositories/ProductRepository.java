a47f331e5810450674bacd0ff1d694373b6d8bf8
Guilherme <guilherme.sousa.castro.2004@gmail.com>
2025-01-24 11:34:18 +0000

    Product

---
+++ src/main/java/com/educandoWeb/cursoSpringBoot/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.educandoWeb.cursoSpringBoot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoWeb.cursoSpringBoot.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+	
+	
+	
+
+}
