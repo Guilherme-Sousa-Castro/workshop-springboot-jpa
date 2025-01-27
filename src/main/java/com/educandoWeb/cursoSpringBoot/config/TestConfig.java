d999cbf21e6c7d54a69ea340a8e3657c42db7bc0
Guilherme <guilherme.sousa.castro.2004@gmail.com>
2025-01-27 10:35:30 +0000

    pt2

--- src/main/java/com/educandoWeb/cursoSpringBoot/config/TestConfig.java
+++ src/main/java/com/educandoWeb/cursoSpringBoot/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoWeb.cursoSpringBoot.entities.Category;
 import com.educandoWeb.cursoSpringBoot.entities.Order;
+import com.educandoWeb.cursoSpringBoot.entities.OrderItem;
 import com.educandoWeb.cursoSpringBoot.entities.Product;
 import com.educandoWeb.cursoSpringBoot.entities.User;
 import com.educandoWeb.cursoSpringBoot.entities.enums.OrderStatus;
 import com.educandoWeb.cursoSpringBoot.repositories.CategoryRepository;
+import com.educandoWeb.cursoSpringBoot.repositories.OrderItemRepository;
 import com.educandoWeb.cursoSpringBoot.repositories.OrderRepository;
 import com.educandoWeb.cursoSpringBoot.repositories.ProductRepository;
 import com.educandoWeb.cursoSpringBoot.repositories.UserRepository;
@@ -35,6 +37,10 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -72,7 +78,12 @@ public class TestConfig implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
 		
 		
---
+++ src/main/java/com/educandoWeb/cursoSpringBoot/repositories/OrderItemRepository.java
@@ -0,0 +1,13 @@
+package com.educandoWeb.cursoSpringBoot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.educandoWeb.cursoSpringBoot.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+	
+	
+	
+	
+
+}
